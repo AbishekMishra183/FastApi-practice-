dc9e8dc7f28c6eb70ba232c723902bfd0aca5e08
abishek Mishra <abishekmishra183@gmail.com>
2025-07-16 15:56:28 +0545

    first commit

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib 
+import numpy as np
+app=FastAPI() 
+model=joblib.load('Saved/DecisionTreePrediction.pkl')
 
-app=FastAPI()
+
+#GUlcose
+ #Bloddpresure
+ #dfp
+#age
+class Patient(BaseModel):
+    gulcose:float
+    bloodpressure:int
+    dfp:float
+    age:int
 
 @app.get('/')         # @ is used to make the route
 
@@ -11,6 +25,36 @@ def show():
 def about():
     return{'Display:','Broad AI is the educational sector'}
 
+
+
+
+
+@app.post('/predict')
+async def predict(data:Patient):
+    input_data=np.array([[
+        data.gulcose,
+        data.bloodpressure,
+        data.dfp,
+        
+
+    ]])
+    predict=await model.predict(input_data)[0]
+    return{'prediction':int(predict)}
+
+
+    #GUlcose
+    #Bloddpresure
+    #dfp
+    #age
+
+
+
+
+
+
+
+
+
 '''
 to run this code we use the 
  in the cmd =>   uvicorn file name: app 
@@ -20,6 +64,8 @@ to run this code we use the
  to see the documentation 
 
 http://127.0.0.1:8000/docs in the url / docs is used 
+
+to give the data to the fastapi pydantic
 
 '''
 
